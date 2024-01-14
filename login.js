3febb7199efdfd7e35946bcdc8b75363c4ba4148
Willy <willy718066@gmail.com>
2024-01-14 17:55:47 +0800

    third practice

---
+++ login.js
@@ -0,0 +1,24 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+      // user內資料記得依API格式、名稱填寫 否則會出錯
+    };
+  },
+  methods: {
+    login() {
+      axios
+        .post("https://ec-course-api.hexschool.io/v2/admin/signin", this.user)
+        .then((res) => {
+          const { expired, token } = res.data;
+          window.location = "product.html";
+        })
+        .catch((err) => alert(err.response.data.message));
+    },
+  },
+}).mount("#app");
---
+++ product.js
@@ -0,0 +1,40 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      APIUrl: "https://ec-course-api.hexschool.io/v2",
+      products: [],
+      tempProduct: {},
+    };
+  },
+  methods: {
+    seeDetails(product) {
+      this.tempProduct = product;
+    },
+    checkLogin() {
+      axios
+        .post(`${this.APIUrl}/api/user/check`)
+        .then((res) => this.getData())
+        .catch((err) => {
+          alert(err.response.data.message);
+          window.location = "login.html";
+        });
+    },
+    getData() {
+      axios
+        .get(`${this.APIUrl}/api/aca101139/admin/products`)
+        .then((res) => (this.products = res.data.products))
+        .catch((err) => alert(err.response.data.message));
+    },
+  },
+  mounted() {
+    const token = document.cookie.replace(
+      /(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/,
+      "$1"
+    );
+    axios.defaults.headers.common.Authorization = token;
+
+    this.checkLogin();
+  },
+}).mount("#app");
